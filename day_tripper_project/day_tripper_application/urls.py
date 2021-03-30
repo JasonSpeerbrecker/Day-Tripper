131c7be44b1903fe3a84852450bfc25661214e77
KHayakawa212 <khayakawa212@gmail.com>
2021-03-30 14:21:06 -0700

    added lots of html placeholders and new methods in views

--- day_tripper_project/day_tripper_application/urls.py
+++ day_tripper_project/day_tripper_application/urls.py
@@ -5,10 +5,23 @@ urlpatterns = [
 
     # Display Routes
     path('', views.index),
-    path('success', views.success),
+    path('dashboard', views.display_dashboard),
+    # path('edit/<int:id>', views.update_trip),
+    path('new', views.display_make_new_trip),
+    # path('detail/<int:id>', views.trip_detail),
+    path('my_trips', views.display_my_trips),
+    path('new_trail', views.display_make_new_trail),
 
     # Action Routes
     path('register', views.register),
     path('login', views.login),
     path('logout', views.logout),
+
+    # path('remove/<int:id>', views.remove),
+    # path('update/<int:id>', views.update),
+    path('create', views.create),
+    path('create_trail', views.create_trail),
+    # path('cancel/<int:id>', views.cancel),
+    # path('join/<int:id>', views.join),
+
 ]
--- day_tripper_project/day_tripper_application/views.py
+++ day_tripper_project/day_tripper_application/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, redirect
-from .models import User
+from .models import User, Trail, Trip
 from django.contrib import messages
 import bcrypt
 
 ###########################################  Display Methods  ###########################################
 def index(request):
     if 'user_id' in request.session:
-        return redirect('/success')
+        return redirect('/dashboard')
     return render(request, "index.html")
 
-#def display_dashboard(request):
-    # if 'user_id' not in request.session:
-    #     return redirect('/')
-    # context = {
-    #     'this_user': User.objects.get(id=request.session['user_id'])
-    # }
-    # return render(request, "dashboard.html", context)
+def display_dashboard(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'this_user': User.objects.get(id=request.session['user_id']),
+        'all_trails': Trail.objects.all()
+    }
+    return render(request, "dashboard_placeholder.html", context)
 
 #def display_trails_details(request, id):
     # if 'user_id' not in request.session:
@@ -26,29 +27,111 @@ def index(request):
     # }
     #return render(request, "detail.html", context)
 
-#def display_my_trips(request):
-    # if 'user_id' not in request.session:
-    #     return redirect('/')
-    # context = {
-    #     'this_user': User.objects.get(id=request.session['user_id'])
-    # }
-    # return render(request, "my_trips.html", context)
+def display_my_trips(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'this_user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, "my_trips_placeholder.html", context)
 
-#def display_make_new_trip(request):
+def display_make_new_trip(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'this_user': User.objects.get(id=request.session['user_id']),
+        'all_trails': Trail.objects.all()
+    }
+    return render(request, "create_trip.html", context)
+
+def display_make_new_trail(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'this_user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, "admin_create_trail.html", context)
+
+#def update_trip(request, id):
     # if 'user_id' not in request.session:
     #     return redirect('/')
     # context = {
     #     'this_user': User.objects.get(id=request.session['user_id'])
+    #     'this_trip': Trip.objects.get(id=id)
     # }
-    # return render(request, "create_trip.html", context)
+    # return render(request, "update_trip.html", context)
 
 ###########################################  Action Methods  ###########################################
 
-#def create(request):
+def create(request):
     user = User.objects.get(id=request.session['user_id'])
+    add_trail = Trail.objects.get(id=request.POST['trail'])
+    # errors = Trip.objects.trip_validator(request.POST)
+    # if len(errors) > 0:
+    #     for key, value in errors.items():
+    #         messages.error(request, value)
+    #     return redirect('/new')
+    new_trip = Trip.objects.create(
+        trip_date = request.POST['trip_date'],
+        food_list = request.POST['food_list'],
+        gear_check = request.POST['gear_check'],
+        creator = user,
+    )
+    new_trip.trails.add(add_trail)
+    return redirect('/my_trips')
+
+def create_trail(request):
+    user = User.objects.get(id=request.session['user_id'])
+    # errors = Trip.objects.trip_validator(request.POST)
+    # if len(errors) > 0:
+    #     for key, value in errors.items():
+    #         messages.error(request, value)
+    #     return redirect('/new')
+    new_trail = Trail.objects.create(
+        name = request.POST['name'],
+        location = request.POST['location'],
+        difficulty = request.POST['difficulty'],
+        distance = request.POST['distance'],
+        elevation_change = request.POST['elevation_change'],
+        route_type = request.POST['route_type'],
+        rating = request.POST['rating'],
+        description = request.POST['description']
+    )
+    return redirect('/dashboard')
+
+# def update(request, id):
+#     errors = Trip.objects.trip_validator(request.POST)
+#     if len(errors) > 0:
+#         for key, value in errors.items():
+#             messages.error(request, value)
+#         return redirect(f'/edit/{id}')
+#     this_trip = Trip.objects.get(id=id)
+#     if request.POST['trip_date'] != this_trip.trip_date:
+#         this_trip.trip_date=request.POST['trip_date']
+#         this_trip.save()
+#     if request.POST['food_list'] != this_trip.food_list:
+#         this_trip.food_list=request.POST['food_list']
+#         this_trip.save()
+#     if request.POST['gear_check'] != this_trip.gear_check:
+#         this_trip.gear_check=request.POST['gear_check']
+#         this_trip.save()
+#     return redirect('/my_trips')
 # 
-# 
-# 
+# def cancel(request, id):
+#     remove_trip = Trip.objects.get(id=id)
+#     remove_user = request.session['user_id']
+#     remove_trip.joined.remove(remove_user)
+#     return redirect('/my_trips')
+
+# def join(request, id):
+#     join_trip = Trip.objects.get(id=id)
+#     join_user = request.session['user_id']
+#     join_trip.joined.add(join_user)
+#     return redirect('/my_trips')
+
+# def remove(request, id):
+#     trip_delete = Trip.objects.get(id=id).delete()
+#     return redirect('/my_trips')
 # 
 # 
 # 
@@ -67,7 +150,7 @@ def index(request):
 #         user = post_user,
 #         comment = request.POST['comment']
 #     )
-#     return redirect('/')
+#     return redirect(f'/detail/{id}')
 
 def register(request):
     # Validations
@@ -90,7 +173,7 @@ def register(request):
     )
     # Start User's session by saving their id to request.session['user_id']
     request.session['user_id'] = new_user.id
-    return redirect('/success')
+    return redirect('/dashboard')
 
 def login(request):
     # create variable user_matching_email to save the instance of User that matches the entered email
@@ -102,7 +185,7 @@ def login(request):
         if bcrypt.checkpw(request.POST['password'].encode(), user_matching_email.password.encode()):
             #if email and password match start users session
             request.session['user_id'] = user_matching_email.id
-            return redirect('/success')
+            return redirect('/dashboard')
         else:
             #this else is if password does not match
             messages.add_message(request, messages.ERROR, 'Invalid Credentials')
